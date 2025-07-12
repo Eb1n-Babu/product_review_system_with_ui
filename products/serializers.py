0b3d7494596e53fdf7099e0517c52e733e08b485
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-12 23:03:35 +0530

    Make created_by field read-only in ProductSerializer to fix required field error

--- products/serializers.py
+++ products/serializers.py
@@ -6,11 +6,11 @@ from reviews.models import Review
 class ProductSerializer(serializers.ModelSerializer):
     average_rating = serializers.SerializerMethodField()
     review_count = serializers.SerializerMethodField()
+    created_by = serializers.PrimaryKeyRelatedField(read_only=True)  # Make read-only
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'created_by', 'created_at', 'updated_at', 'average_rating',
-                  'review_count']
+        fields = ['id', 'name', 'description', 'price', 'created_by', 'created_at', 'updated_at', 'average_rating', 'review_count']
 
     def get_average_rating(self, obj):
         reviews = obj.reviews.all()
